4d9d7d8ea17c3e35bf000ea714a8250d1681703a
martybonacci <martybonacci@gmail.com>
2020-09-02 17:06:04 -0600

    updated setupProxyExample

--- frontend/src/setupProxyExample.js
+++ frontend/src/setupProxyExample.js
@@ -3,9 +3,8 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
 	app.use(proxy('/apis', {
 		logLevel: 'debug',
-		target: "https://YOUR-IP-ADDRESS/:8080/",
+		target: "http://YOUR-IP-ADDRESS/:8080/",
 		changeOrigin: true,
-		secure: true,
-
+		// secure: true,
 	}));
 };
