bfc235f2526e52a11a38f703c983a1a56a578a60
gkephart <gkephart@cnm.edu>
2020-09-08 10:34:07 -0600

    quick bug fix.

--- backend/src/controllers/profile.controller.ts
+++ backend/src/controllers/profile.controller.ts
@@ -7,34 +7,43 @@ import {body} from "express-validator";
 import {updateProfile} from "../../utils/profile/updateProfile";
 
 export async function putProfileController(request: Request, response: Response) {
-  const {profileId}  = request.body
-  const partialProfile: PartialProfile = request.body
-  const profileIdFromSession: string = <string>request.session?.profile.profileId
+  try {
 
-  const preformUpdate = async (partialProfile: PartialProfile) => {
-    const previousProfile: Profile = await selectWholeProfileByProfileId(<string>partialProfile.profileId)
-    const newProfile : Profile = { ...previousProfile,  ...partialProfile }
-    console.log("updated profile", newProfile)
-    await updateProfile(newProfile)
 
-    return response.json({status: 200, data: null, message: "Profile successfully updated"})
+    const {profileId} = request.params
+    const partialProfile: PartialProfile = request.body
+    const profileIdFromSession: string = <string>request.session?.profile.profileId
+
+    const preformUpdate = async (partialProfile: PartialProfile) => {
+      const previousProfile: Profile = await selectWholeProfileByProfileId(<string>partialProfile.profileId)
+      const newProfile: Profile = {...previousProfile, ...partialProfile}
+      await updateProfile(newProfile)
+      return response.json({status: 200, data: null, message: "Profile successfully updated"})
+    }
+
+    const preformFailedUpdate = (message: string) => {
+      return response.json({status: 418, data: null, message})
+    }
+
+    return profileId === profileIdFromSession
+      ? preformUpdate(partialProfile)
+      : preformFailedUpdate("you are not allowed to preform this action")
+  } catch (error) {
+    console.error(error)
   }
-
-  const preformFailedUpdate = (message: string ) => {
-    return response.json({status:418, data: null, message})
-  }
-
- return  profileId === profileIdFromSession
-   ? preformUpdate(partialProfile)
-   : preformFailedUpdate("you are not allowed to preform this action")
 
 }
 
 export async function getProfileByProfileId(request: Request, response: Response) {
+  try {
+    const {profileId} = request.params;
+    const mySqlResult = await selectPartialProfileByProfileId(profileId);
+    const data = mySqlResult ?? null
+    const status: Status = {status: 200, data, message: null}
+    return response.json(status)
 
-  const {profileId} = request.params;
-  const mySqlResult = await selectPartialProfileByProfileId(profileId);
-  const data = mySqlResult ?? null
-  const status: Status = {status: 200, data, message: null}
-  return response.json(status)
+  } catch (error) {
+    console.error(error)
+  }
+
 }
