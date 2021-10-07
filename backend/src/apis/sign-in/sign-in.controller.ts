124e90f9314456fa1d40d729bedb58f85fc11e6d
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-10-07 15:30:14 -0600

    got signin to work

--- backend/src/apis/sign-in/sign-in.controller.ts
+++ backend/src/apis/sign-in/sign-in.controller.ts
@@ -1,32 +1,38 @@
 import {NextFunction, Request, Response} from 'express';
 import "express-session";
-// import passport from 'passport';
-// import passportLocal, {Strategy} from 'passport-local';
 
 import uuid from "uuid";
 import {generateJwt, validatePassword} from "../../utils/auth.utils";
 import {Profile} from "../../utils/interfaces/Profile";
 import {selectProfileByProfileEmail} from "../../utils/profile/selectProfileByProfileEmail";
 
+
+
 export async function signInController(request: Request, response: Response, nextFunction: NextFunction): Promise<Response | undefined> {
-
     try {
-
-        const {profilePassword} = request.body;
-
-        const {profileEmail} = request.body
-
-        const getProfileData = async () => {
-            const data = await selectProfileByProfileEmail(profileEmail)
-            return data ? data : {message: 'Incorrect username or password'}
-        };
-
-        // console.log("profile info", profile)
-
-
         const authenticate = async () => {
-            // console.log(profile)
-            const {profileId, profileAtHandle, profileAvatarUrl, profileEmail, profilePhone} = profile;
+
+            const {profilePassword} = request.body;
+
+            const {profileEmail} = request.body
+            const mySqlResult = await selectProfileByProfileEmail(profileEmail);
+
+            console.log("my sql result", mySqlResult)
+
+            // @ts-ignore
+            const {profileId, profileAtHandle, profileAvatarUrl, profilePhone, profileHash, profileActivationToken} = mySqlResult
+
+            const profile: Profile = {
+                profileId,
+                profileAtHandle,
+                profileAvatarUrl,
+                profileEmail,
+                profilePhone,
+                profileHash,
+                profileActivationToken
+            }
+            console.log("profile info", profile)
+
             const signature: string = uuid();
             const authorization: string = generateJwt({
                 profileId,
@@ -70,86 +76,7 @@ export async function signInController(request: Request, response: Response, nex
 
         return authenticate()
 
-        // const sendInfo = async (profile: Profile) => {
-        //     await authenticate(profile)
-        //     return nextFunction
-        // }
-
-        // await sendInfo(profile)
-
     } catch (error: any) {
         return response.json({status: 500, data: null, message: error.message})
     }
 }
-
-
-//         passport.authenticate(
-//             'local',
-//             {session: false},
-//             async (err: any, passportUser: Profile) => {
-//                 console.log(passportUser)
-//                 const {profileId, profileAtHandle, profileAvatarUrl, profileEmail, profilePhone} = passportUser;
-//                 const signature: string = uuid();
-//                 const authorization: string = generateJwt({
-//                     profileId,
-//                     profileAtHandle,
-//                     profileAvatarUrl,
-//                     profileEmail,
-//                     profilePhone
-//                 }, signature);
-//
-//                 const signInFailed = (message: string) => response.json({
-//                     status: 400,
-//                     data: null,
-//                     message
-//                 });
-//
-//                 const signInSuccessful = () => {
-//
-//                     // commented out for testing purposes
-//                     // if(passportUser.profileActivationToken !== null) {
-//                     // 	signInFailed("please activate your account")
-//                     // }
-//
-//                     if (request.session) {
-//                         request.session.profile = passportUser;
-//                         request.session.jwt = authorization;
-//                         request.session.signature = signature;
-//                     }
-//
-//                     response.header({
-//                         authorization
-//                     });
-//
-//                     return response.json({status: 200, data: null, message: "sign in successful"})
-//                 };
-//
-//
-//                 const isPasswordValid: boolean = passportUser && await validatePassword(passportUser.profileHash, profilePassword);
-//
-//                 return isPasswordValid ? signInSuccessful() : signInFailed("Invalid email or password");
-//             })(request, response, nextFunction)
-//     } catch (error: any) {
-//         return response.json({status: 500, data: null, message: error.message})
-//     }
-// }
-
-//
-// const LocalStrategy = passportLocal.Strategy;
-//
-// export const passportStrategy: Strategy = new LocalStrategy(
-//     {
-//         usernameField: 'profileEmail',
-//         passwordField: "profilePassword"
-//     },
-//     async (email, password, done) => {
-//         try {
-//
-//             const profile: Profile | null = await selectProfileByProfileEmail(email);
-//
-//             return profile ? done(null, profile) : done(undefined, undefined, {message: 'Incorrect username or password'});
-//         } catch (error) {
-//             return done(error);
-//         }
-//     }
-// );
