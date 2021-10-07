f9fe5982d4fb9b5728775e58c84f5d1960e1b2f5
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-10-07 10:23:39 -0600

    aet up authenticate logic

--- backend/src/apis/sign-in/sign-in.controller.ts
+++ backend/src/apis/sign-in/sign-in.controller.ts
@@ -1,81 +1,155 @@
 import {NextFunction, Request, Response} from 'express';
 import "express-session";
-import passport from 'passport';
-import passportLocal, {Strategy} from 'passport-local';
+// import passport from 'passport';
+// import passportLocal, {Strategy} from 'passport-local';
 
 import uuid from "uuid";
 import {generateJwt, validatePassword} from "../../utils/auth.utils";
 import {Profile} from "../../utils/interfaces/Profile";
 import {selectProfileByProfileEmail} from "../../utils/profile/selectProfileByProfileEmail";
 
-export async function signInController(request: Request, response: Response, nextFunction: NextFunction):Promise<Response|undefined> {
+export async function signInController(request: Request, response: Response, nextFunction: NextFunction): Promise<Response | undefined> {
 
-  try {
+    try {
 
-    const {profilePassword} = request.body;
+        const {profilePassword} = request.body;
 
+        const {profileEmail} = request.body
 
-    passport.authenticate(
-      'local',
-      {session: false},
-      async (err: any, passportUser: Profile) => {
-        console.log(passportUser)
-        const {profileId, profileAtHandle, profileAvatarUrl, profileEmail, profilePhone} = passportUser;
-        const signature: string = uuid();
-        const authorization: string = generateJwt({profileId, profileAtHandle, profileAvatarUrl, profileEmail, profilePhone}, signature);
-
-        const signInFailed = (message: string) => response.json({
-          status: 400,
-          data: null,
-          message
-        });
-
-        const signInSuccessful = () => {
-
-          // commented out for testing purposes
-          // if(passportUser.profileActivationToken !== null) {
-          // 	signInFailed("please activate your account")
-          // }
-
-          if (request.session) {
-            request.session.profile = passportUser;
-            request.session.jwt = authorization;
-            request.session.signature = signature;
-          }
-
-          response.header({
-            authorization
-          });
-
-          return response.json({status: 200, data: null, message: "sign in successful"})
+        const getProfileData = async () => {
+            const data = await selectProfileByProfileEmail(profileEmail)
+            return data ? data : {message: 'Incorrect username or password'}
         };
 
+        // console.log("profile info", profile)
 
-        const isPasswordValid: boolean = passportUser && await validatePassword(passportUser.profileHash, profilePassword);
 
-        return isPasswordValid ? signInSuccessful() : signInFailed("Invalid email or password");
-      })(request, response, nextFunction)
-  } catch (error: any) {
-    return response.json({status: 500, data: null, message: error.message})
-  }
+        const authenticate = async () => {
+            // console.log(profile)
+            const {profileId, profileAtHandle, profileAvatarUrl, profileEmail, profilePhone} = profile;
+            const signature: string = uuid();
+            const authorization: string = generateJwt({
+                profileId,
+                profileAtHandle,
+                profileAvatarUrl,
+                profileEmail,
+                profilePhone
+            }, signature);
+
+            const signInFailed = (message: string) => response.json({
+                status: 400,
+                data: null,
+                message
+            });
+
+            const signInSuccessful = () => {
+
+                // commented out for testing purposes
+                // if(profile.profileActivationToken !== null) {
+                // 	signInFailed("please activate your account")
+                // }
+
+                if (request.session) {
+                    request.session.profile = profile;
+                    request.session.jwt = authorization;
+                    request.session.signature = signature;
+                }
+
+                response.header({
+                    authorization
+                });
+
+                return response.json({status: 200, data: null, message: "sign in successful"})
+            };
+
+
+            const isPasswordValid: boolean = profile && await validatePassword(profile.profileHash, profilePassword);
+
+            return isPasswordValid ? signInSuccessful() : signInFailed("Invalid email or password");
+        }
+
+        return authenticate()
+
+        // const sendInfo = async (profile: Profile) => {
+        //     await authenticate(profile)
+        //     return nextFunction
+        // }
+
+        // await sendInfo(profile)
+
+    } catch (error: any) {
+        return response.json({status: 500, data: null, message: error.message})
+    }
 }
 
 
-const LocalStrategy = passportLocal.Strategy;
+//         passport.authenticate(
+//             'local',
+//             {session: false},
+//             async (err: any, passportUser: Profile) => {
+//                 console.log(passportUser)
+//                 const {profileId, profileAtHandle, profileAvatarUrl, profileEmail, profilePhone} = passportUser;
+//                 const signature: string = uuid();
+//                 const authorization: string = generateJwt({
+//                     profileId,
+//                     profileAtHandle,
+//                     profileAvatarUrl,
+//                     profileEmail,
+//                     profilePhone
+//                 }, signature);
+//
+//                 const signInFailed = (message: string) => response.json({
+//                     status: 400,
+//                     data: null,
+//                     message
+//                 });
+//
+//                 const signInSuccessful = () => {
+//
+//                     // commented out for testing purposes
+//                     // if(passportUser.profileActivationToken !== null) {
+//                     // 	signInFailed("please activate your account")
+//                     // }
+//
+//                     if (request.session) {
+//                         request.session.profile = passportUser;
+//                         request.session.jwt = authorization;
+//                         request.session.signature = signature;
+//                     }
+//
+//                     response.header({
+//                         authorization
+//                     });
+//
+//                     return response.json({status: 200, data: null, message: "sign in successful"})
+//                 };
+//
+//
+//                 const isPasswordValid: boolean = passportUser && await validatePassword(passportUser.profileHash, profilePassword);
+//
+//                 return isPasswordValid ? signInSuccessful() : signInFailed("Invalid email or password");
+//             })(request, response, nextFunction)
+//     } catch (error: any) {
+//         return response.json({status: 500, data: null, message: error.message})
+//     }
+// }
 
-export const  passportStrategy: Strategy = new LocalStrategy(
-  {
-    usernameField: 'profileEmail',
-    passwordField: "profilePassword"
-  },
-  async (email, password, done) => {
-    try {
-
-      const profile: Profile | null = await selectProfileByProfileEmail(email);
-
-      return profile ? done(null, profile) : done(undefined, undefined, {message: 'Incorrect username or password'});
-    } catch (error: any) {
-      return done(error);
-    }
-  }
-  );
+//
+// const LocalStrategy = passportLocal.Strategy;
+//
+// export const passportStrategy: Strategy = new LocalStrategy(
+//     {
+//         usernameField: 'profileEmail',
+//         passwordField: "profilePassword"
+//     },
+//     async (email, password, done) => {
+//         try {
+//
+//             const profile: Profile | null = await selectProfileByProfileEmail(email);
+//
+//             return profile ? done(null, profile) : done(undefined, undefined, {message: 'Incorrect username or password'});
+//         } catch (error) {
+//             return done(error);
+//         }
+//     }
+// );
