31911032937cc26e46cbcf1c4c02ee75e6e4800e
George Kephart <gkephart@cnm.edu>
2022-02-23 14:41:24 -0700

    minor clean up

--- backend/src/utils/profile/insertProfile.ts
+++ backend/src/utils/profile/insertProfile.ts
@@ -6,6 +6,7 @@ export async function insertProfile(profile: Profile) : Promise<string>{
 		const mysqlConnection = await connect();
 		const query : string = 'INSERT INTO profile(profileId, profileActivationToken, profileAtHandle, profileAvatarUrl,  profileEmail, profileHash, profilePhone ) VALUES (UUID_TO_BIN(UUID()) , :profileActivationToken, :profileAtHandle, :profileAvatarUrl, :profileEmail, :profileHash, :profilePhone)';
 		await mysqlConnection.execute(query, profile);
+		await mysqlConnection.end()
 		return 'Profile Successfully Created'
 	} catch (error) {
 		throw error
--- backend/src/utils/tweet/insertTweet.ts
+++ backend/src/utils/tweet/insertTweet.ts
@@ -5,7 +5,7 @@ import {ResultSetHeader, RowDataPacket} from 'mysql2';
 export async function insertTweet(tweet: Tweet) : Promise<string> {
   try {
     const mySqlConnection = await connect()
-    const mySqlQuery = "INSERT INTO tweet(tweetId, tweetProfileId, tweetContent, tweetDate ) VALUES(UUID_TO_BIN(UUID()), UUID_TO_BIN(:tweetProfileId), :tweetContent, NOW())";
+    const mySqlQuery = "INSERT INTO tweet(tweetId, tweetProfileId, tweetContent, tweetDate ) VALUES(UUID_TO_BIN(UUID()), UUID_TO_BIN(:tweetProfileId))";
 
    const [result]= await mySqlConnection.execute(mySqlQuery, tweet) as [ResultSetHeader, RowDataPacket]
     return "Tweet created successfully"
