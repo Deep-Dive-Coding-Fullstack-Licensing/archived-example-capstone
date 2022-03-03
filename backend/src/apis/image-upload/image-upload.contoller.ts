24f150ae0278ce1534e426883ce8eb66b3ae5628
George Kephart <gkephart@cnm.edu>
2022-03-03 16:03:05 -0700

    removed streamifier and cleaned up the code

--- backend/src/apis/image-upload/image-upload.contoller.ts
+++ backend/src/apis/image-upload/image-upload.contoller.ts
@@ -1,13 +1,16 @@
-import {NextFunction, Request, Response} from "express";
-import {uploadToCloudinary} from "../../utils/cloudinary.utils";
+import { Request, Response } from 'express'
+import { uploadToCloudinary } from '../../utils/cloudinary.utils'
 
-export async function imageUploadController(request: Request, response: Response, nextFunction: NextFunction) {
+export async function imageUploadController (request: Request, response: Response): Promise<Response> {
   try {
     // uncomment if in production
-   // const message : string = await uploadToCloudinary(request)
-    const message : string =  "http://placekitten.com/150/150"
-    return response.json({status: 200, data: null, message: message})
+    if (request.file === undefined) {
+      throw new Error('Please provide a valid file type ')
+    }
+
+    const message: string = await uploadToCloudinary(request.file)
+    return response.json({ status: 200, data: null, message: message })
   } catch (error: any) {
-    return response.json({status:400, message: error.message, data: null})
+    return response.json({ status: 400, message: error.message, data: null })
   }
 }
--- backend/src/utils/controllers/multer.controller.ts
+++ backend/src/utils/controllers/multer.controller.ts
@@ -1,18 +1,18 @@
-import multer, {StorageEngine} from "multer";
-import {Express, Request} from "express";
+import multer, { FileFilterCallback, StorageEngine } from 'multer'
+import { Express, Request } from 'express'
 
 /*
  * Single file upload handler that uses multer and multer memory storage to handle file validation
  * @see https://medium.com/@joeokpus/uploading-images-to-cloudinary-using-multer-and-expressjs-f0b9a4e14c54
  */
 
-const storage : StorageEngine = multer.memoryStorage()
-const limits  = {fields: 2, files: 1, parts: 2 }
-const fileFilter  = ( request: Request, file : Express.Multer.File , callback : any) => {
-  const {originalname} = file
-  return originalname.match(/\.(jpg|jpeg|png|gif)$/)
+const storage: StorageEngine = multer.memoryStorage()
+const limits = { fields: 2, files: 1, parts: 2 }
+const fileFilter = (request: Request, file: Express.Multer.File, callback: FileFilterCallback): void => {
+  const { originalname } = file
+  return (originalname.match(/\.(jpg|jpeg|png|gif)$/) != null)
     ? callback(null, true)
-    : callback(new Error("only images are allowed to be uploaded"), false)
+    : callback(null, false)
 }
 
-export const imageUploader = multer({storage, limits, fileFilter}).single('image')
+export const imageUploader = multer({ storage, limits, fileFilter }).single('image')
