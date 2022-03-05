fc70d79f755dcd6fd3442659f6bbe5d949a7629f
George Kephart <gkephart@cnm.edu>
2022-03-05 14:43:09 -0700

    cleaned up the sign in controller to make it more readable for studnets

--- backend/src/apis/sign-in/sign-in.controller.ts
+++ backend/src/apis/sign-in/sign-in.controller.ts
@@ -1,73 +1,44 @@
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import 'express-session'
 import { v4 as uuid } from 'uuid'
 import { generateJwt, validatePassword } from '../../utils/auth.utils'
 import { Profile } from '../../utils/interfaces/Profile'
 import { selectProfileByProfileEmail } from '../../utils/profile/selectProfileByProfileEmail'
 
-export async function signInController (request: Request, response: Response): Promise<Response | undefined> {
-  const { profileEmail } = request.body
-  const mySqlResult: Profile | null = await selectProfileByProfileEmail(profileEmail)
-  const isEmailValid: boolean = (mySqlResult != null)
-
+export async function signInController (request: Request, response: Response): Promise<Response> {
   try {
-    const authenticate = async () => {
-      const { profilePassword } = request.body
+    const { profileEmail, profilePassword } = request.body
+    const profile: Profile | null = await selectProfileByProfileEmail(profileEmail)
 
-      // @ts-expect-error isEmailValid determines mySqlResult will not be null
-      const { profileId, profileAtHandle, profileAvatarUrl, profilePhone, profileHash, profileActivationToken } = mySqlResult
-
-      const profile: Profile = {
-        profileId,
-        profileAtHandle,
-        profileAvatarUrl,
-        profileEmail,
-        profilePhone,
-        profileHash,
-        profileActivationToken
-      }
-
-      const signature: string = uuid()
-      const authorization: string = generateJwt({
-        profileId,
-        profileAtHandle,
-        profileAvatarUrl,
-        profileEmail,
-        profilePhone
-      }, signature)
-
-      const signInFailed = (message: string) => response.json({
-        status: 400,
-        data: null,
-        message
-      })
-
-      const signInSuccessful = () => {
-        // commented out for testing purposes
-        // if(profile.profileActivationToken !== null) {
-        // 	signInFailed("please activate your account")
-        // }
-
-        if (request.session) {
-          request.session.profile = profile
-          request.session.jwt = authorization
-          request.session.signature = signature
-        }
-
-        response.header({
-          authorization
-        })
-
-        return response.json({ status: 200, data: null, message: 'sign in successful' })
-      }
-
-      const isPasswordValid: boolean = profile && await validatePassword(profile.profileHash, profilePassword)
-
-      return isPasswordValid ? signInSuccessful() : signInFailed('Invalid email or password')
-    }
-
-    return isEmailValid ? await authenticate() : response.json({ status: 400, data: null, message: 'Invalid email or password' })
+    return (profile != null) && await validatePassword(profile.profileHash, profilePassword)
+      ? signInSuccessful(request, response, profile)
+      : signInFailed(response)
   } catch (error: any) {
     return response.json({ status: 500, data: null, message: error.message })
   }
+}
+
+function signInFailed (response: Response): Response {
+  return response.json({ status: 400, message: 'Email or password is incorrect please try again.', data: null })
+}
+
+function signInSuccessful (request: Request, response: Response, profile: Profile): Response {
+  const { profileId, profileAtHandle, profileAvatarUrl, profileEmail, profilePhone } = profile
+  const signature: string = uuid()
+  const authorization: string = generateJwt({
+    profileId,
+    profileAtHandle,
+    profileAvatarUrl,
+    profileEmail,
+    profilePhone
+  }, signature)
+
+  request.session.profile = profile
+  request.session.jwt = authorization
+  request.session.signature = signature
+
+  response.header({
+    authorization
+  })
+  return response.json({ status: 200, message: 'Sign in successful', data: null })
 }
