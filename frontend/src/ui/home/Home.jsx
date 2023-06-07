bf7a2d3fd3e36eeaa694fc702a8fbf8acb6c9d32
George Kephart <gkephart@cnm.edu>
2023-06-07 17:34:21 -0600

    got the frontend to use typescript javascript(incremental adoption) and vite.

---
+++ frontend/src/ui/home/Home.jsx
@@ -0,0 +1,36 @@
+import React, {useEffect} from 'react';
+import {useSelector, useDispatch} from "react-redux";
+import {TweetCard} from "./TweetCard.jsx";
+import {TweetForm} from "./TweetForm.jsx";
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import {fetchAllTweets} from "../../store/tweets";
+
+export const Home = () => {
+
+  const tweets = useSelector(state => state.tweets ? state.tweets : []);
+
+  const dispatch = useDispatch();
+  const effects = () => {
+    dispatch(fetchAllTweets());
+  };
+  useEffect(effects, [dispatch]);
+
+  return (
+    <>
+      <Container>
+        <Button onClick={()=> {
+
+          dispatch(fetchAllTweets())}}>Click Me</Button>
+        <Row className='pt-2 border border-light border-2'>
+          <Col>
+            <TweetForm/>
+          </Col>
+        </Row>
+        {tweets.map(tweet => <TweetCard
+          tweet={tweet} key={tweet.tweetId}/>)}
+      </Container>
+    </>
+  )
+};
+
+
